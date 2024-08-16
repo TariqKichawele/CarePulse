ddcd5b1e7edbcd523f2b358ca1c6992a60cebb9d
TariqKichawele <tariqkichawele66@gmail.com>
2024-08-16 13:14:28 -0400

    Implemented admin page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases, messaging } from "../appwrite.config";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
 import { formatDateTime } from "../utils";
+import { Appointment } from "@/types/appwrite.types";
 
 export const createAppointment = async(appointment: CreateAppointmentParams) => {
     try {
@@ -70,6 +71,50 @@ export const getAppointment = async (appointmentId: string) => {
         return parseStringify(appointment);
     } catch (error) {
         console.error("An error occurred while retrieving appointment:", error);
+    }
+}
+
+export const getRecentAppointmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc("$createdAt")]
+        );
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        };
+
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+                switch (appointment.status) {
+                    case 'scheduled':
+                        acc.scheduledCount++;
+                        break;
+                    case 'pending':
+                        acc.pendingCount++;
+                        break;
+                    case 'cancelled':
+                        acc.cancelledCount++;
+                        break;
+                }
+                return acc;
+            },
+            initialCounts
+        );
+
+        const data = {
+            totalCount: appointments.total,
+            ...counts,
+            documents: appointments.documents
+        };
+
+        return parseStringify(data);
+    } catch (error) {
+        console.error("An error occurred while retrieving appointment list:", error);
     }
 }
 
